da2637197e3256083700253e1623db3cf0792b33
Razana062 <120932051+Ishrat0628@users.noreply.github.com>
2024-12-22 22:15:48 +0530

    Update keBoard.java

--- keBoard.java
+++ keBoard.java
@@ -13,3 +13,18 @@ class keBoard
 		System.out.println("sum is\n"+c);
 	}
 }
+import java.lang.*;
+import java.util.*;
+class keBoard
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int a,b,c;
+		System.out.println("Enter two number");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=a*b;
+		System.out.println("Multiplication is\n"+c);
+	}
+}
