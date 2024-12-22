42d17202167a5a5ec464322d628d82ee96e33b42
Razana <mdrazamkandar1@gmail.com>
2024-12-22 22:05:19 +0530

    version1

---
+++ keBoard.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.util.*;
+class keBoard
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int a,b,c;
+		System.out.println("Enter two number");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=a+b;
+		System.out.println("sum is\n"+c);
+	}
+}
---
+++ printName.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.util.*;
+
+class printName
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		String name;
+		System.out.println("May I Know your name");
+		name=sc.nextLine();
+		System.out.println("Welcom "+name);
+		
+	}
+}
